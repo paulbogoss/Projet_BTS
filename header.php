9bfba1a144b2bf7309c1fd05d2bc425d561d1650
paulbogoss <paul.thobla1@gmail.com>
2022-03-01 16:59:15 +0100

    ça avance

--- header.php
+++ header.php
@@ -10,5 +10,10 @@
     <script src="https://kit.fontawesome.com/da7397688c.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Vamos a la playa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button></header>
 <body>
-
--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <?php
 include("header.php");
 ?>
+<div class="container">
 <h1>Créer une plage</h1>
 <form action="post">
-
+    <div class="form-group">
+        <label for="nom">Nom de la plage</label>
+        <input type="text" class="form-control" id="nom" placeholder="Nom de la plage">
+    </div>
 </form>
+</div>
 <?php
 include ("footer.php");
